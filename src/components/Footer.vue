585fbad8304eed62b57886cc3f905c0a528ee512
Nemanja <nemanja.avramovic@partech.com>
2025-11-04 19:31:47 +0100

    removed footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,10 +9,7 @@
         Not in any way associated with Netflix. For entertainment purposes only. This site does not host any videos.
       </p></li>
       <li><p>
-      Assembled by <a class="text-white underline" href="https://x.com/avramator" rel="nofollow" target="_blank">Avram</a>,
-        using <a class="text-white underline" href="https://imdbapi.dev/" rel="nofollow" target="_blank">IMDb API</a>
-        and <a class="text-white underline" href="https://vidsrcme.su" rel="nofollow" target="_blank">VidSrc</a> for streaming.
-        Use of ad blockers is highly recommended.
+      Assembled by Avram. Use of ad blockers is highly recommended.
       </p></li>
       <li>
     </li>
